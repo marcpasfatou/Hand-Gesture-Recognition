34e14461e23f18ac4dacc65dd35a24b811bb320e
marcpasfatou <dinhm1@unlv.nevada.edu>
2019-03-17 23:29:52 -0700

    Add files via upload
    Backup

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace HandGesture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.original = new System.Windows.Forms.PictureBox();
+            this.crop = new System.Windows.Forms.PictureBox();
+            this.histogram = new AForge.Controls.Histogram();
+            this.histogram1 = new AForge.Controls.Histogram();
+            this.openvideo = new System.Windows.Forms.Button();
+            this.stopvideo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.original)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(1339, 79);
+            this.Start.Margin = new System.Windows.Forms.Padding(4);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(100, 28);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(1444, 79);
+            this.Stop.Margin = new System.Windows.Forms.Padding(4);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(100, 28);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(1444, 47);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(102, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1336, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Camera";
+            // 
+            // original
+            // 
+            this.original.Location = new System.Drawing.Point(664, 15);
+            this.original.Margin = new System.Windows.Forms.Padding(4);
+            this.original.Name = "original";
+            this.original.Size = new System.Drawing.Size(659, 480);
+            this.original.TabIndex = 5;
+            this.original.TabStop = false;
+            // 
+            // crop
+            // 
+            this.crop.Location = new System.Drawing.Point(1339, 126);
+            this.crop.Margin = new System.Windows.Forms.Padding(4);
+            this.crop.Name = "crop";
+            this.crop.Size = new System.Drawing.Size(219, 158);
+            this.crop.TabIndex = 6;
+            this.crop.TabStop = false;
+            // 
+            // histogram
+            // 
+            this.histogram.Location = new System.Drawing.Point(1339, 307);
+            this.histogram.Margin = new System.Windows.Forms.Padding(4);
+            this.histogram.Name = "histogram";
+            this.histogram.Size = new System.Drawing.Size(219, 91);
+            this.histogram.TabIndex = 8;
+            this.histogram.Text = "histogram";
+            this.histogram.Values = null;
+            // 
+            // histogram1
+            // 
+            this.histogram1.Location = new System.Drawing.Point(1339, 405);
+            this.histogram1.Margin = new System.Windows.Forms.Padding(4);
+            this.histogram1.Name = "histogram1";
+            this.histogram1.Size = new System.Drawing.Size(219, 89);
+            this.histogram1.TabIndex = 9;
+            this.histogram1.Text = "histogram1";
+            this.histogram1.Values = null;
+            // 
+            // openvideo
+            // 
+            this.openvideo.Location = new System.Drawing.Point(1339, 15);
+            this.openvideo.Margin = new System.Windows.Forms.Padding(4);
+            this.openvideo.Name = "openvideo";
+            this.openvideo.Size = new System.Drawing.Size(100, 28);
+            this.openvideo.TabIndex = 10;
+            this.openvideo.Text = "Video File";
+            this.openvideo.UseVisualStyleBackColor = true;
+            this.openvideo.Click += new System.EventHandler(this.openvideo_Click_1);
+            // 
+            // stopvideo
+            // 
+            this.stopvideo.Location = new System.Drawing.Point(1457, 15);
+            this.stopvideo.Margin = new System.Windows.Forms.Padding(4);
+            this.stopvideo.Name = "stopvideo";
+            this.stopvideo.Size = new System.Drawing.Size(55, 28);
+            this.stopvideo.TabIndex = 11;
+            this.stopvideo.Text = "stop";
+            this.stopvideo.UseVisualStyleBackColor = true;
+            this.stopvideo.Click += new System.EventHandler(this.stopvideo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(1568, 518);
+            this.Controls.Add(this.stopvideo);
+            this.Controls.Add(this.openvideo);
+            this.Controls.Add(this.histogram1);
+            this.Controls.Add(this.histogram);
+            this.Controls.Add(this.crop);
+            this.Controls.Add(this.original);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Hand Gesture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.original)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.crop)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox original;
+        private System.Windows.Forms.PictureBox crop;
+        private AForge.Controls.Histogram histogram;
+        private AForge.Controls.Histogram histogram1;
+        private System.Windows.Forms.Button openvideo;
+        private System.Windows.Forms.Button stopvideo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using AForge;
+using AForge.Imaging;
+using AForge.Video.FFMPEG;
+
+
+
+namespace HandGesture
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection ListOfCams;
+        private VideoCaptureDevice SelectedCam; //From where we will take image
+        private VideoFileSource videoSource; 
+
+        private HSLFiltering filter = new HSLFiltering();
+        private Opening openingFilter = new Opening();
+        private Threshold thresholdFilter = new Threshold(20);
+        private Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+        private Erosion erosionFilter = new Erosion();
+
+        BlobCounter bc = new BlobCounter();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            
+            ListOfCams = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            
+            if (ListOfCams.Count == 0)
+                return;
+
+            comboBox1.Items.Clear();
+            foreach (FilterInfo Cam in ListOfCams)
+            {
+                comboBox1.Items.Add(Cam.Name);
+            }
+
+            
+
+        }
+
+        private void StopCamera()
+        {
+            if (SelectedCam != null)
+            {
+                SelectedCam.SignalToStop();
+                SelectedCam.Stop();
+            }
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == string.Empty)
+                return;
+            
+            
+            SelectedCam = new VideoCaptureDevice(ListOfCams[comboBox1.SelectedIndex].MonikerString);
+
+            SelectedCam.NewFrame += new NewFrameEventHandler(SelectedCam_NewFrame);
+            histogram.Show();
+            histogram1.Show();           
+            SelectedCam.VideoResolution = SelectedCam.VideoCapabilities[0];
+            SelectedCam.Start();
+        }
+
+        void SelectedCam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+           
+            Bitmap bmp = new Bitmap(100,100);
+            Graphics placeholder = Graphics.FromImage(bmp);
+            placeholder.FillRectangle(Brushes.White,0,0,bmp.Width,bmp.Height);
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap originalImage = (Bitmap)eventArgs.Frame.Clone();
+           
+            
+            // set color ranges to keep red-orange
+            filter.Hue = new IntRange(0, 35);
+            filter.Saturation = new Range(0.19f, 1);
+            //filter.Luminance = new Range(0.31f, 255);
+            // apply the filter
+            filter.ApplyInPlace(image);           
+            openingFilter.ApplyInPlace(image);
+            Bitmap greyimage = grayscaleFilter.Apply(image);
+            thresholdFilter.ApplyInPlace(greyimage);
+            erosionFilter.Apply(greyimage);
+            
+            
+            
+            // process binary image
+            bc.ProcessImage(image);
+            bc.MinWidth = 50;
+            bc.MinHeight = 50;
+            bc.FilterBlobs = true;
+            bc.ObjectsOrder = ObjectsOrder.Size;
+            Rectangle[] rects = bc.GetObjectsRectangles();
+            if (rects.Length > 0)
+            {
+                bmp = new Bitmap(rects[0].Width, rects[0].Height);
+
+                
+                Graphics g = Graphics.FromImage(bmp);
+                g.DrawImage(greyimage, 0, 0, rects[0], GraphicsUnit.Pixel);
+                
+                g.Dispose();
+
+
+
+
+                // process blobs
+                foreach (Rectangle rect in rects)
+                {
+                    Graphics g1 = Graphics.FromImage(originalImage);
+
+
+                    using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+                    {
+                        g1.DrawRectangle(pen, rect);
+                    }
+
+                    g1.Dispose();
+                }
+            }
+
+            Crop cropFilter = new Crop(new Rectangle(0, (int)(0.15 * bmp.Height), bmp.Width, (int)(0.25 * bmp.Height)));
+            
+            
+                Bitmap handImage = cropFilter.Apply(bmp);
+                Bitmap overlay = new Bitmap(handImage.Width, handImage.Height);
+                using (Graphics g = Graphics.FromImage(overlay))
+                {
+                    g.FillRectangle(Brushes.White, 0, 0, overlay.Width, overlay.Height);
+                }
+                Subtract subfilter = new Subtract(handImage);
+                Bitmap resultImage = subfilter.Apply(overlay);
+                HorizontalIntensityStatistics his = new HorizontalIntensityStatistics(handImage);
+                
+
+                double palmlimit = 0.4 * handImage.Height;
+                int[] hisValues = (int[])his.Red.Values.Clone();
+                for (int i = 0; i < handImage.Width; i++)
+                {
+                    hisValues[i] = ((double)hisValues[i] / 255 > palmlimit) ? 1 : 0;
+                }
+
+
+                histogram.Values = his.Red.Values;
+                histogram1.Values = hisValues;
+                //display number of fingers
+
+                Graphics num = Graphics.FromImage(image);
+                string text = "0";
+                if (bmp.Height > 1.5*bmp.Width)                {
+                    text = string.Format(countfingers(hisValues));
+                }
+                else { text = "0"; }
+                Font drawFont = new Font("Courier", 18, FontStyle.Bold);
+                SolidBrush drawBrush = new SolidBrush(Color.White);
+
+                num.DrawString(text, drawFont, drawBrush, new PointF(0, 5));
+
+                drawFont.Dispose();
+                drawBrush.Dispose();
+
+                num.Dispose();
+            
+			// display in picture box
+            pictureBox1.Image = originalImage;
+            original.Image = image;
+            crop.Image = handImage;
+           
+           
+        }
+
+        private void video_NewFrame( object sender, NewFrameEventArgs eventArgs )
+{
+    // get new frame
+    Bitmap bmp = new Bitmap(100, 100);
+    Graphics placeholder = Graphics.FromImage(bmp);
+    placeholder.FillRectangle(Brushes.White, 0, 0, bmp.Width, bmp.Height);
+    Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+
+    // process the frame
+
+    Bitmap originalImage = (Bitmap)eventArgs.Frame.Clone();
+
+
+    // set color ranges to keep red-orange
+    filter.Hue = new IntRange(0, 40);
+    filter.Saturation = new Range(0.19f, 1);
+    //filter.Luminance = new Range(0.31f, 255);
+    // apply the filter
+    filter.ApplyInPlace(image);
+    openingFilter.ApplyInPlace(image);
+    Bitmap greyimage = grayscaleFilter.Apply(image);
+    thresholdFilter.ApplyInPlace(greyimage);
+    erosionFilter.Apply(greyimage);
+
+
+
+    // process binary image
+    bc.ProcessImage(image);
+    bc.MinWidth = 50;
+    bc.MinHeight = 50;
+    bc.FilterBlobs = true;
+    bc.ObjectsOrder = ObjectsOrder.Size;
+    Rectangle[] rects = bc.GetObjectsRectangles();
+    if (rects.Length > 0)
+    {
+        bmp = new Bitmap(rects[0].Width, rects[0].Height);
+
+
+        Graphics g = Graphics.FromImage(bmp);
+        g.DrawImage(greyimage, 0, 0, rects[0], GraphicsUnit.Pixel);
+
+        g.Dispose();
+
+
+
+
+        // process blobs
+        foreach (Rectangle rect in rects)
+        {
+            Graphics g1 = Graphics.FromImage(originalImage);
+
+
+            using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+            {
+                g1.DrawRectangle(pen, rect);
+            }
+
+            g1.Dispose();
+        }
+    }
+
+    Crop cropFilter = new Crop(new Rectangle(0, (int)(0.15 * bmp.Height), bmp.Width, (int)(0.25 * bmp.Height)));
+
+
+    Bitmap handImage = cropFilter.Apply(bmp);
+    Bitmap overlay = new Bitmap(handImage.Width, handImage.Height);
+    using (Graphics g = Graphics.FromImage(overlay))
+    {
+        g.FillRectangle(Brushes.White, 0, 0, overlay.Width, overlay.Height);
+    }
+    Subtract subfilter = new Subtract(handImage);
+    Bitmap resultImage = subfilter.Apply(overlay);
+    HorizontalIntensityStatistics his = new HorizontalIntensityStatistics(handImage);
+
+
+    double palmlimit = 0.35 * handImage.Height;
+    int[] hisValues = (int[])his.Red.Values.Clone();
+    for (int i = 0; i < handImage.Width; i++)
+    {
+        hisValues[i] = ((double)hisValues[i] / 255 > palmlimit) ? 1 : 0;
+    }
+
+
+    histogram.Values = his.Red.Values;
+    histogram1.Values = hisValues;
+    //display number of fingers
+
+    Graphics num = Graphics.FromImage(image);
+    string text = "0";
+    if (bmp.Height > 1.6 * bmp.Width)
+    {
+        text = string.Format(countfingers(hisValues));
+    }
+    else { text = "0"; }
+    Font drawFont = new Font("Courier", 18, FontStyle.Bold);
+    SolidBrush drawBrush = new SolidBrush(Color.White);
+
+    num.DrawString(text, drawFont, drawBrush, new PointF(0, 5));
+
+    drawFont.Dispose();
+    drawBrush.Dispose();
+
+    num.Dispose();
+
+    // display in picture box
+    pictureBox1.Image = originalImage;
+    original.Image = image;
+    crop.Image = handImage;
+           
+           
+           
+}
+
+        private string countfingers(int[] his)
+        {
+            int num = 0;
+            int stride = 0;
+            int blank = 0;
+            string position = "";
+            for (int i = 0; i < his.Length-1; i++)
+            {
+                if (his[i] == 1 ) stride += 1;
+                if (his[i] == 0) blank += 1;
+                
+                if (his[i] - his[i + 1] != 0)
+                {
+                     num += 1; ;
+                     if (stride / his.Length > 0.25) position = "closed";
+                     stride = 0;
+
+                }
+                
+            }
+            num = num / 2;
+            return num.ToString() + position;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            StopCamera();
+        }
+
+               
+        private void openvideo_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            if (Dialog.ShowDialog() == DialogResult.OK)
+            {
+                videoSource = new VideoFileSource(Dialog.FileName);
+                // set NewFrame event handler
+                videoSource.NewFrame += new NewFrameEventHandler(video_NewFrame);
+                // start the video source
+                videoSource.Start();
+                
+            }
+        }
+
+        private void stopvideo_Click(object sender, EventArgs e)
+        {
+            if (videoSource != null) videoSource.Stop();
+        }
+
+        
+
+    }
+}
